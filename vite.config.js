1f21698f36d7de64c16e62726f4abdc06a3d4e01
Yandry Villagomez <rafaeldev.villagomez@gmail.com>
2026-07-29 12:46:37 +0200

    Cambio para despligue arreglo en base url

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import imagemin from 'vite-plugin-imagemin';
 // https://vitejs.dev/config/
 export default defineConfig({
   // 1. BASE URL para subdirectorio de producción
-  base: "/porfolio/",
+  base: "/",
 
   plugins: [
     react(),
